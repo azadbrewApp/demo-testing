bf5391e8c4a0551ab42ae002520d3f2288589af2
azad <azad@thebrewapps.com>
2023-02-26 09:34:56 +0530

    server is running to listen

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    return res.status(200).json('Server is running Azad');
+})
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})

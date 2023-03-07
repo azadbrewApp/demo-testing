af0e8c8b39bc7e00ff9e16753cb12edf0ef747f4
azad <azad@thebrewapps.com>
2023-03-07 18:36:12 +0530

    added

--- controller/userController.js
+++ controller/userController.js
@@ -5,7 +5,7 @@ const uploadDemoFile = (req, res) => {
     const file = req.file;
 
     console.log('FILE', file);
-    return res.status(200).json(file.path)
+    return res.status(200).json(`${req.host}\${file.filename}`)
 }
 
 const storage = multer.diskStorage({
